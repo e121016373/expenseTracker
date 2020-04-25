c60599b805e9011ceb37ff4701e1d33dd0cadcf4
e121016373 <e121016373@hotmail.com>
2020-04-25 01:13:02 -0700

    ui implemented

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
-import { ExpenseList } from './components/ExpenseList';
-import { AddExpense } from './components/AddExpense';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 import { GlobalProvider } from './context/GlobalState';
 
@@ -12,12 +12,12 @@ import './App.css';
 function App() {
   return (
     <GlobalProvider>
-      <Header/>
       <div className="container">
+        <Header/>
         <Balance/>
         <IncomeExpenses/>
-        <ExpenseList/>
-        <AddExpense/>
+        <TransactionList/>
+        <AddTransaction/>
       </div>
     </GlobalProvider>
   );
--- client/src/components/IncomeExpenses.js
+++ client/src/components/IncomeExpenses.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const IncomeExpenses = () => {
+    const { transactions } = useContext(GlobalContext);
+    const plus_total = transactions
+        .map(transaction => transaction.amount < 0 ? 0 : transaction.amount)
+        .reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const minus_total = transactions
+        .map(transaction => transaction.amount < 0 ? transaction.amount : 0)
+        .reduce((acc, item) => (acc += item), 0).toFixed(2);
+
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p className="money plus">+$0.00</p>
+                <p className="money plus">+${plus_total}</p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p className="money minus">-$0.00</p>
+                <p className="money minus">-${Math.abs(minus_total)}</p>
             </div>
         </div>
     )
